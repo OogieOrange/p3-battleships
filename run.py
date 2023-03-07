07749c6ac6e5e2145b87c6133e20b8529538ae21
OogieOrange <evelinapalffy01@gmail.com>
2023-03-07 13:57:19 +0000

    Add generate ships function

--- run.py
+++ run.py
@@ -1,15 +1,30 @@
-def playing_board():
+from random import randint
+
+PLAYER_BOARD = [[" " for num in range(6)] for num in range(6)]
+GUESS_BOARD = [[" " for num in range(6)] for num in range(6)]
+
+def playing_board(board):
     """
     Creates the vitual playing board
     """
-    print("   A  B  C  D  E  F")
-    print("+++++++++++++++++++++")
-    board = [["| " for num in range(6)] for num in range(6)]
+    print("  A  B  C  D  E  F")
+    print("++++++++++++++++++++")
     row_num = 1
     for row in board:
-        print(row_num, " ".join(row))
+        print(row_num, " |".join(row))
         row_num += 1
-    print("+++++++++++++++++++++")
+    print("++++++++++++++++++++")
 
 
-playing_board()
+def gen_ships(board):
+    for ship in range(5):
+        ship_row = randint(0, 5)
+        ship_column = randint(0, 5)
+        while board[ship_row][ship_column] == "o":
+            ship_row = randint(0, 5)
+            ship_column = randint(0, 5)
+        board[ship_row][ship_column] = "o"
+
+gen_ships(PLAYER_BOARD)
+playing_board(PLAYER_BOARD)
+playing_board(GUESS_BOARD)
